122198460a5234e7829257dd13c7a28656339199
ggrummer <69330507+ggrummer@users.noreply.github.com>
2020-08-25 16:06:07 -0400

    Added Output Files
    Added files needed for quick custom displays.

---
+++ patterns/bitmapper.bat
@@ -0,0 +1,2 @@
+C:/Tools/lscc/iCEcube2.2017.08/sbt_backend/bin/win32/opt\bitmap.exe C:\Tools\lscc\iCEcube2.2017.08\sbt_backend\devices\ICE40T05.dev --design ..\netlist\oadb-star_top --device_name iCE40UP5K --package SG48 --outdir ..\outputs\bitmap --low_power on --init_ram on --init_ram_bank 1111 --frequency low --warm_boot on
+timeout /t 6
